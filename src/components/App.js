24b63e51250313972ed1e237b32a149210a9fb71
Alexei Kubasov <kubasovalexei@yandex.ru>
2021-07-14 17:12:14 +0700

    feat: сообщение об ошибке, если обратиться к api не удалось

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,38 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
 import { today, tomorrowDate } from '../utils/date';
 import * as api from '../utils/api';
 import './App.css';
 
 function App() {
 
-  api.takeData(today, tomorrowDate).then((res) => console.log(res))
+  const [spaceObjects, setSpaceObjects] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState('')
+
+  useEffect(() => {
+    api.takeData(today, tomorrowDate)
+    .then((res) => {
+      console.log(res)
+      setIsLoading(false)
+      setSpaceObjects(res.near_earth_objects[`${today}`])
+    })
+    .catch((err) => {
+      console.log(err)
+      setError(err)
+    })
+  }, [])
+
+  console.log(spaceObjects)
 
   return (
-    <div className="App">
+    <div className='App'>
+      {isLoading ? 
+      <>
+      <div className='App__loader'></div>
+      <p className='App__error'>{error}</p>
+      </> : 
+      <p>no</p>}
     </div>
   );
 }
