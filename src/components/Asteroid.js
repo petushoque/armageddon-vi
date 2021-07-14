621d0bd898a4701d6c473ff345721cfba4dc2d63
Alexei Kubasov <kubasovalexei@yandex.ru>
2021-07-14 20:16:29 +0700

    feat: краткая информация о астероиде

--- src/components/Asteroid.js
+++ src/components/Asteroid.js
@@ -5,8 +5,32 @@ export default function Asteroid(props) {
     console.log(props)
 
     return (
-        <div className='Asteroid'>
-            <h2>{props.asteroid && props.asteroid.id}</h2>
+        <div key={props.asteroid && props.asteroid.id} className='Asteroid'>
+            <div className='Asteroid__illustration'>
+                <div className='Asteroid__illustration_statue'></div>
+                <h2 className='Asteroid__title'>{props.asteroid && props.asteroid.name}</h2>
+            </div>
+            <div className='Asteroid__information'>
+                <div className='Asteroid__information_row'>
+                    <p className='Asteroid__paragraph'>Дата</p>
+                    <p className='Asteroid__information_dots'></p>
+                    <p className='Asteroid__paragraph'>{props.asteroid && props.asteroid.close_approach_data[0].close_approach_date_full}</p>
+                </div>
+
+                <div className='Asteroid__information_row'>
+                    <p className='Asteroid__paragraph'>Диаметр, м</p>
+                    <p className='Asteroid__information_dots'></p>
+                    <p className='Asteroid__paragraph'>{props.asteroid && props.asteroid.estimated_diameter.meters.estimated_diameter_max}</p>
+                </div>
+
+                <div className='Asteroid__information_row'>
+                    <p className='Asteroid__paragraph'>Расстояние, км</p>
+                    <p className='Asteroid__information_dots'></p>
+                    <p className='Asteroid__paragraph'>{props.asteroid && props.asteroid.close_approach_data[0].miss_distance.kilometers}</p>
+                </div>
+
+            </div>
+            
         </div>
     )
 }
