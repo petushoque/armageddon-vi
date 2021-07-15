bd97ce3d240e14c1f109673e6ed03e6197296307
Alexei Kubasov <kubasovalexei@yandex.ru>
2021-07-15 11:34:13 +0700

    feat: схематичный астероид

--- src/components/Asteroid.js
+++ src/components/Asteroid.js
@@ -7,6 +7,7 @@ export default function Asteroid(props) {
     return (
         <div key={props.asteroid && props.asteroid.id} className='Asteroid'>
             <div className='Asteroid__illustration'>
+                <div className='Asteroid__illustration_asteroid'></div>
                 <div className='Asteroid__illustration_statue'></div>
                 <h2 className='Asteroid__title'>{props.asteroid && props.asteroid.name}</h2>
             </div>
