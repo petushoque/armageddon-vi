c660d9ffac433f8fa8d71bf9dad29f5319f03ddb
Alexei Kubasov <kubasovalexei@yandex.ru>
2021-07-15 10:55:37 +0700

    feat: уникальные ключи для каждого астероида

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,8 +4,8 @@ import Asteroid from './Asteroid';
 export default function Main(props) {
     return (
         <div className='Main'>
-        {props.asteroids && props.asteroids.map((asteroid) => (
-            <Asteroid asteroid={asteroid}/>
+        {props.asteroids && props.asteroids.map((asteroid) => (           
+                <Asteroid key={asteroid.id} asteroid={asteroid}/>
         ))}
         </div>
     )
