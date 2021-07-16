37a0459c4c395539e1e6731f6aee79dfba2dfa20
Alexei Kubasov <kubasovalexei@yandex.ru>
2021-07-16 18:10:02 +0700

    feat: поправить класс к элементе с кнопкой

--- src/elements/DestroyButton.js
+++ src/elements/DestroyButton.js
@@ -4,7 +4,7 @@ export default function DestroyButton() {
     return (
         <a href="" data-tooltip="600Кб">
             <span>
-                <em>Скачать</em><i class="fa fa-cloud-download"></i>
+                <em>Скачать</em><i className="fa fa-cloud-download"></i>
             </span>
         </a>
     )
