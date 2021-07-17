49ff93140f0f868748d8e7ca5ac3318c6f9a3035
Alexei Kubasov <kubasovalexei@yandex.ru>
2021-07-17 08:32:18 +0700

    feat: убрать анимаию рокеты

--- src/elements/DestroyButton.js
+++ src/elements/DestroyButton.js
@@ -4,7 +4,7 @@ export default function DestroyButton() {
     return (
         <a href="" data-tooltip="600Кб">
             <span>
-                <em>Скачать</em><i className="fa fa-cloud-download">&#128640;</i>
+                <em>Поехали!</em><i className="fa fa-cloud-download">&#128640;</i>
             </span>
         </a>
     )
