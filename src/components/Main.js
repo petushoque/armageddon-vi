949822a2f2cd0853a5fd6c4e8bd93cb226fe1599
Alex Kubasov <kubasovalexei@yandex.ru>
2021-07-25 07:48:19 +0700

    feat: import useState module to Maim.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,12 @@
+import React from 'react';
+import { useState } from 'react';
 import './Main.css';
 import Asteroid from './Asteroid';
 
 export default function Main(props) {
+
+    
+
     return (
         <div className='Main'>
         {props.asteroids && props.asteroids.map((asteroid) => (           
